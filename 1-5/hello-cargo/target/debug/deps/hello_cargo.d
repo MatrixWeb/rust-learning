7b432b2dcbd075f3aa3896ea9d871d848e361a36
林宜斌 <454033517@qq.com>
2021-10-02 10:31:01 +0800

    clean obj

--- 1-5/hello-cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-/Users/matrixlin/Documents/rust_proj/rust-learning/1-5/hello-cargo/target/debug/deps/hello_cargo: src/main.rs
-
-/Users/matrixlin/Documents/rust_proj/rust-learning/1-5/hello-cargo/target/debug/deps/hello_cargo.d: src/main.rs
-
-src/main.rs:
--- 1-5/hello-cargo/target/debug/hello-cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/matrixlin/Documents/rust_proj/rust-learning/1-5/hello-cargo/target/debug/hello-cargo: /Users/matrixlin/Documents/rust_proj/rust-learning/1-5/hello-cargo/src/main.rs
