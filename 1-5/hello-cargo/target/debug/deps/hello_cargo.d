37359ccdf06593213e6057f8679b444a3f96277e
林宜斌 <454033517@qq.com>
2021-10-02 10:30:14 +0800

    add hello cargo

---
+++ 1-5/hello-cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/matrixlin/Documents/rust_proj/rust-learning/1-5/hello-cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/matrixlin/Documents/rust_proj/rust-learning/1-5/hello-cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ 1-5/hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/Users/matrixlin/Documents/rust_proj/rust-learning/1-5/hello-cargo/target/debug/hello-cargo: /Users/matrixlin/Documents/rust_proj/rust-learning/1-5/hello-cargo/src/main.rs
