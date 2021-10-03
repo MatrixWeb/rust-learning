3dd7903cdd030bca007b0d890c2a7610b9485534
林宜斌 <454033517@qq.com>
2021-10-03 14:53:59 +0800

    add copy and return

--- 6-1/manage-memory/src/main.rs
+++ 6-1/manage-memory/src/main.rs
@@ -1,3 +1,6 @@
+#[derive(Debug)]
+struct Highlight<'document>(&'document str);
+
 fn main() {
     println!("Hello, world!");
     // ownership
@@ -7,8 +10,32 @@ fn main() {
     let mut greeting = String::from("hello");
     change(&mut greeting);
     println!("greeting: {}", greeting);
+    // lifetime
+    let magic2 = String::from("shen");
+    {
+        let magic1 = String::from("yuan");
+        let result;
+        {
+            result = longest_word(&magic2, &magic1);
+        }
+        println!("the longest word: {}", result);
+    }
+    println!("magic2 {}", magic2);
+    let text = String::from("The quick brown fox jumps over the lazy dog.");
+    let fox = Highlight(&text[4..19]);
+    let dog = Highlight(&text[35..43]);
+    println!("{:?}", fox);
+    println!("{:?}", dog);
 }
 
 fn process(s: String) { println!("use {}", s);}
 
 fn change(s: &mut String) {s.push_str(", world");}
+
+fn longest_word<'a>(x: &'a String, y: &'a String) -> &'a String {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
